080aa11f5371d5a4b41d2f4939b2ab03f2b7e1a9
shaka912 <khalnayek15@gmail.com>
2024-07-15 21:34:34 +0500

    added support for translation on sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -16,6 +16,7 @@ import { schema } from "./sanity/schema";
 import { structure } from "./sanity/structure";
 import { colorInput } from "@sanity/color-input";
 import { linkField } from "sanity-plugin-link-field";
+import { documentInternationalization } from "@sanity/document-internationalization";
 export default defineConfig({
   basePath: "/studio",
   projectId,
@@ -31,6 +32,15 @@ export default defineConfig({
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
+    documentInternationalization({
+      supportedLanguages: [
+        { id: "es", title: "Spanish" },
+        { id: "en", title: "English" },
+        { id: "ar", title: "Arabic" },
+      ],
+      schemaTypes: ["page", "collections", "header-footer"],
+      languageField: `language`,
+    }),
     linkField({
       linkableSchemaTypes: ["header-footer"],
     }),
